1301d861595af745377e03bbb47b8c981b0aa5aa
Bex Warner <rw1048a@student.american.edu>
2016-09-14 13:12:05 -0400

    fixed program to included values from .5 to 1 not just 1

--- bex_warner_discrete_hw1.py
+++ bex_warner_discrete_hw1.py
@@ -1,7 +1,7 @@
 isNotFinished = True
-print "This program will return to you the potential m values, given n,"
+print "This program will return to you the potential n values, given m,"
 while isNotFinished:
-    input = raw_input('Enter n: ')
+    input = raw_input('Enter m: ')
     try:
         int(input)
     except ValueError:
@@ -12,5 +12,22 @@ while isNotFinished:
         output.append(input)
         if (0 - input != input):
             output.append(0 - input)
+        otherValuesFound = True
+        negative = False
+        if input <= 0:
+            negative = True
+            x = input - 1
+        else:
+            x = input + 1
+        while otherValuesFound:
+            if float(input)/x > 0.5 or float(input)/x == 0.5:
+                output.append(x)
+                output.append(-x)
+                if negative:
+                    x = x - 1
+                else:
+                    x = x + 1
+            else:
+                otherValuesFound = False
         print output
         isNotFinished = False
