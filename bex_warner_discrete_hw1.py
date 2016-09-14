48c35a8aee57a945584ad3d1122f3f0e82d3f074
Bex Warner <rw1048a@student.american.edu>
2016-09-14 13:19:33 -0400

    commented code for clarity

--- bex_warner_discrete_hw1.py
+++ bex_warner_discrete_hw1.py
@@ -2,32 +2,39 @@ isNotFinished = True
 print "This program will return to you the potential n values, given m,"
 while isNotFinished:
     input = raw_input('Enter m: ')
+    #checks if input was an integer
     try:
         int(input)
     except ValueError:
         print "That was not an acceptable integer."
+    # re-asks for input in the int wasn't a real int and restarts at top of while loop
     else:
         input = int(input)
         output = []
         output.append(input)
-        if (0 - input != input):
-            output.append(0 - input)
+        #adds input to output list and inverse of input
+        if (-input != input):
+            output.append(-input)
         otherValuesFound = True
         negative = False
+        # checks if number is negative so that x will be scaled in the right direction
         if input <= 0:
             negative = True
-            x = input - 1
+            n = input - 1
         else:
-            x = input + 1
+            n = input + 1
         while otherValuesFound:
-            if float(input)/x > 0.5 or float(input)/x == 0.5:
-                output.append(x)
-                output.append(-x)
+            if float(input)/n > 0.5 or float(input)/n == 0.5:
+                output.append(n)
+                output.append(-n)
                 if negative:
-                    x = x - 1
+                    n = n - 1
                 else:
-                    x = x + 1
+                    n = n + 1
+                # adds x to output while it's less than or equal to .05
             else:
                 otherValuesFound = False
+                # exits while loop when x no longer meets the conditions
         print output
         isNotFinished = False
+        # prints output and quits main program while loop
