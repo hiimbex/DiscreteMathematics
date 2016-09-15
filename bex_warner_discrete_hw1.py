b345451ffcd9cbd436da9993a0f9f2c02db169b7
Bex Warner <rw1048a@student.american.edu>
2016-09-15 18:23:00 -0400

    Cleaned up some things

--- bex_warner_discrete_hw1.py
+++ bex_warner_discrete_hw1.py
@@ -16,25 +16,18 @@ while isNotFinished:
         if (-input != input):
             output.append(-input)
         otherValuesFound = True
-        negative = False
-        # checks if number is negative so that x will be scaled in the right direction
-        if input <= 0:
-            negative = True
-            n = input - 1
-        else:
-            n = input + 1
+        input = abs(input)
+        n = input + 1
+        print input
         while otherValuesFound:
             if float(input)/n > 0.5 or float(input)/n == 0.5:
                 output.append(n)
                 output.append(-n)
-                if negative:
-                    n = n - 1
-                else:
-                    n = n + 1
-                # adds x to output while it's less than or equal to .05
+                n += 1
+                # adds n to output while it's less than or equal to .05
             else:
                 otherValuesFound = False
-                # exits while loop when x no longer meets the conditions
+                # exits while loop when n no longer meets the conditions
         print output
         isNotFinished = False
         # prints output and quits main program while loop
