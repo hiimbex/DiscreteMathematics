ac5301dbd6cfc55cf4a29c64e3b3a8a8943b7947
Bex Warner <rw1048a@student.american.edu>
2016-10-05 11:50:49 -0400

    added an implementation of the matching brackets stack push/pop problem in python

---
+++ practicebrackets.py
@@ -0,0 +1,20 @@
+def is_matched(strOfBrackets):
+    stack = []
+    pushChars, popChars = "<({[", ">)}]"
+    for c in strOfBrackets :
+        if c in pushChars :
+            stack.append(c)
+        elif c in popChars :
+            if not len(stack) :
+                return False
+            else :
+                stackTop = stack.pop()
+                balancingBracket = pushChars[popChars.index(c)]
+                if stackTop != balancingBracket :
+                    return False
+        else :
+            return False
+    return not len(stack)
+
+print is_matched("{{[[((([)))]]}}")
+print is_matched("{{[[((([])))]]}}")
